712b670fe8462927b25222fe123c43c3f25c3d0d
Shota <sh.kuroda0524@gmail.com>
2018-11-30 16:42:07 +0700

    [Add] Add NewRequestAdapter.kt and setup override functions

---
+++ app/src/main/java/lentborrow/cs3231/com/lentborrow/customCell/requestCell/NewRequestAdapter.kt
@@ -0,0 +1,29 @@
+package lentborrow.cs3231.com.lentborrow.customCell.requestCell
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import lentborrow.cs3231.com.lentborrow.R
+import lentborrow.cs3231.com.lentborrow.controller.database.request.Request
+
+class NewRequestAdapter(val requests: ArrayList<Request>) : RecyclerView.Adapter<NewRequestAdapter.ViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return requests.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.request_customcell, parent, false)
+        val newView = ViewHolder(v)
+        return newView
+    }
+
+    override fun onBindViewHolder(viewHolder: NewRequestAdapter.ViewHolder, p1: Int) {
+
+    }
+
+    class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+
+    }
+}
