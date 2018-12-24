0f3fce5503c2d55d85d6cf69aee3af94607fb1e5
navin20 <navinrow@gmail.com>
2018-12-24 17:08:25 +0700

    clickable reject button

--- app/src/main/java/lentborrow/cs3231/com/lentborrow/RequestDetailActivity.kt
+++ app/src/main/java/lentborrow/cs3231/com/lentborrow/RequestDetailActivity.kt
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.widget.LinearLayout
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_request_box.*
+import kotlinx.android.synthetic.main.activity_request_detail.*
 import lentborrow.cs3231.com.lentborrow.controller.database.request.RequestController
 import lentborrow.cs3231.com.lentborrow.controller.localValue.LocalValueController
 import lentborrow.cs3231.com.lentborrow.customCell.requestCell.NewRequestAdapter
@@ -15,29 +17,46 @@ class RequestDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_request_detail)
-        title = "RequestDetail"
-        val mCon = MessageController(this)
 
-        val rCon = RequestController()
-        val lvCon = LocalValueController(this)
+        refuseRequest.setOnClickListener {
+            Toast.makeText(this, "rejected", Toast.LENGTH_LONG)
 
-        rCon.getRequestsByOwnerID(lvCon.getID(), { requests ->
-            val filtered = rCon.filterByWaitingRequests(requests)
-            if (rCon.filterByRejectedRequests(requests) == filtered) {
-                mCon.showToast("Rejected")
+        }
+        acceptRequest.setOnClickListener {
+            Toast.makeText(this,"accepted",Toast.LENGTH_LONG)
 
-            } else if (rCon.filterByAcceptedRequests(requests) == filtered) {
-                mCon.showToast("Accepted")
-                val rView = newReqestList
-                rView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
-                var adapter = NewRequestAdapter(filtered)
-                rView.adapter = adapter
 
-            }
-        }, { error ->
-            mCon.showToast("Error happen when get request.")
-        })
+        }
     }
 }
 
+
+
+
+//
+//        title = "RequestDetail"
+//        val mCon = MessageController(this)
+//
+//        val rCon = RequestController()
+//        val lvCon = LocalValueController(this)
+//
+//        rCon.getRequestsByOwnerID(lvCon.getID(), { requests ->
+//            val filtered = rCon.filterByWaitingRequests(requests)
+//            if (rCon.filterByRejectedRequests(requests) == filtered) {
+//                mCon.showToast("Rejected")
+//
+//            } else if (rCon.filterByAcceptedRequests(requests) == filtered) {
+//                mCon.showToast("Accepted")
+//                val rView = newReqestList
+//                rView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+//                var adapter = NewRequestAdapter(filtered)
+//                rView.adapter = adapter
+//
+//            }
+//        }, { error ->
+//            mCon.showToast("Error happen when get request.")
+//        })
+//    }
+//}
+//
 
