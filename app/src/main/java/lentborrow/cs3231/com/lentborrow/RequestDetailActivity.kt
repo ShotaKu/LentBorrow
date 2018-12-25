cf6bfa55e94b5dfe8df53b5e3cbaf007c96ab0b1
Shota <sh.kuroda0524@gmail.com>
2018-12-25 21:07:26 +0700

    [Update] Write onCreate function for get request data from firebase

--- app/src/main/java/lentborrow/cs3231/com/lentborrow/RequestDetailActivity.kt
+++ app/src/main/java/lentborrow/cs3231/com/lentborrow/RequestDetailActivity.kt
@@ -3,11 +3,15 @@ package lentborrow.cs3231.com.lentborrow
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.view.View
 import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_request_box.*
+import kotlinx.android.synthetic.main.activity_request_detail.*
+import lentborrow.cs3231.com.lentborrow.controller.database.book.BookController
 import lentborrow.cs3231.com.lentborrow.controller.database.request.RequestController
 import lentborrow.cs3231.com.lentborrow.controller.localValue.LocalValueController
 import lentborrow.cs3231.com.lentborrow.customCell.requestCell.NewRequestAdapter
+import lentborrow.cs3231.com.lentborrow.generic.ImageDownloader
 import lentborrow.cs3231.com.lentborrow.generic.MessageController
 
 class RequestDetailActivity : AppCompatActivity() {
@@ -18,24 +22,36 @@ class RequestDetailActivity : AppCompatActivity() {
         title = "RequestDetail"
         val mCon = MessageController(this)
 
+        val requestID = intent.getStringExtra("requestID");
         val rCon = RequestController()
-        val lvCon = LocalValueController(this)
+        rCon.getRequestByRequestID(requestID,{ request ->
+            if(request != null){
+                val bCon = BookController()
+                bCon.getBookByID(request.bookID,{book ->
+                    if(book != null){
+                        bCon.getBookByID(request.tradeWithID,{ tradeWithBook ->
+                            if(tradeWithBook != null){
+                                val bookImageDownload = ImageDownloader(book.imageURL,yourBookImage)
+                                bookImageDownload.startDownload()
+                                val tradeWithDownload = ImageDownloader(tradeWithBook.imageURL, requesterBookImage)
+                                tradeWithDownload.startDownload()
 
-        rCon.getRequestsByOwnerID(lvCon.getID(), { requests ->
-            val filtered = rCon.filterByWaitingRequests(requests)
-            if (rCon.filterByRejectedRequests(requests) == filtered) {
-                mCon.showToast("Rejected")
+                                yourBook.text = book.name
+                                requesterBook.text = "At\n" + tradeWithBook.name
 
-            } else if (rCon.filterByAcceptedRequests(requests) == filtered) {
-                mCon.showToast("Accepted")
-                val rView = newReqestList
-                rView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
-                var adapter = NewRequestAdapter(filtered)
-                rView.adapter = adapter
-
+                                tradeAt.text = book.locate
+                                tradeOn.text = request.date + "\n" + request.time
+                            }
+                        },{error ->
+                            mCon.showToast(error.message)
+                        })
+                    }
+                },{error ->
+                    mCon.showToast(error.message)
+                })
             }
-        }, { error ->
-            mCon.showToast("Error happen when get request.")
+        },{ error ->
+            mCon.showToast(error.message)
         })
     }
 }
