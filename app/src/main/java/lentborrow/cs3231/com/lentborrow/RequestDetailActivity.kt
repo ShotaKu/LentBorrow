0ea89b9084f05e22888ada4fd977621650611d56
Shota <sh.kuroda0524@gmail.com>
2018-12-26 14:47:41 +0700

    [Update] Hide action bar

--- app/src/main/java/lentborrow/cs3231/com/lentborrow/RequestDetailActivity.kt
+++ app/src/main/java/lentborrow/cs3231/com/lentborrow/RequestDetailActivity.kt
@@ -1,13 +1,14 @@
 package lentborrow.cs3231.com.lentborrow
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import android.widget.LinearLayout
-import kotlinx.android.synthetic.main.activity_request_box.*
+import android.support.v7.app.AppCompatActivity
+import com.google.firebase.database.DatabaseError
+import kotlinx.android.synthetic.main.activity_request_detail.*
+import lentborrow.cs3231.com.lentborrow.controller.database.book.Book
+import lentborrow.cs3231.com.lentborrow.controller.database.book.BookController
 import lentborrow.cs3231.com.lentborrow.controller.database.request.RequestController
 import lentborrow.cs3231.com.lentborrow.controller.localValue.LocalValueController
-import lentborrow.cs3231.com.lentborrow.customCell.requestCell.NewRequestAdapter
+import lentborrow.cs3231.com.lentborrow.generic.ImageDownloader
 import lentborrow.cs3231.com.lentborrow.generic.MessageController
 
 class RequestDetailActivity : AppCompatActivity() {
@@ -17,21 +18,40 @@ class RequestDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_request_detail)
         title = "RequestDetail"
         val mCon = MessageController(this)
+        val requestid = intent.getStringExtra("requestID")
 
         val rCon = RequestController()
+
         val lvCon = LocalValueController(this)
+        val bCon = BookController()
 
-        rCon.getRequestsByOwnerID(lvCon.getID(), { requests ->
-            val filtered = rCon.filterByWaitingRequests(requests)
-            if (rCon.filterByRejectedRequests(requests) == filtered) {
-                mCon.showToast("Rejected")
+        rCon.getRequestByRequestID(requestid, { request ->
 
-            } else if (rCon.filterByAcceptedRequests(requests) == filtered) {
-                mCon.showToast("Accepted")
-                val rView = newReqestList
-                rView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
-                var adapter = NewRequestAdapter(filtered)
-                rView.adapter = adapter
+            if (request != null) {
+                bCon.getBookByID(request.bookID, { book: Book? ->
+                    if (book != null) {
+                        bCon.getBookByID(request.tradeWithID, { Trade: Book? ->
+                            if (Trade != null) {
+                                val bookimage = ImageDownloader(book!!.imageURL, yourBookImage)
+                                bookimage.startDownload()
+                                val requestimage = ImageDownloader(Trade!!.imageURL, requesterBookImage)
+                                requestimage.startDownload()
+                                requesterBook.text = Trade.name.toString()
+                                yourBook.text = book.name
+                                tradeAt.text =  "At"+"\n"+book.locate
+                                tradeOn.text = "On"+"\n"+request.time+"\n"+request.date
+
+                            }
+
+
+                        }, { error: DatabaseError ->
+
+                        })
+                    }
+
+                }, { error: DatabaseError ->
+
+                })
 
             }
         }, { error ->
